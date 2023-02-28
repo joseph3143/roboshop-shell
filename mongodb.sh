157d598225d7a075521329327fff64ab17e8b66b
joseph3143 <121853912+joseph3143@users.noreply.github.com>
2023-02-28 16:36:41 +0530

    code changes

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,15 @@
+source common.sh
+
+print_head "Setup mongodb Repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Install mongodb"
 yum install mongodb-org -y
+
+print_head "Enable Mongodb"
 systemctl enable mongod
+
+print_head "Start Mongodb Service"
 systemctl start mongod
 
 # Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
