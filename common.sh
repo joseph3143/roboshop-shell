cd9906ea20d6f0b26879671a46b3e214a882c3f0
joseph3143 <121853912+joseph3143@users.noreply.github.com>
2023-03-04 14:50:04 +0530

    Updating roboshop-shell

--- common.sh
+++ common.sh
@@ -16,7 +16,21 @@ status_check() {
   fi
 }
 
-NODEJS(){
+schema_setup(){
+  if [ "${schema_type}" == "mongo" ]; then
+    print_head "Copy MongoDB Repo File "
+    cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+    status_check $?
+
+    print_head "Install Mongo Client"
+    yum install mongodb-org-shell -y &>>${log_file}
+    status_check $?
+
+    print_head "Load Schema"
+    mongo --host mongodb.devopsculture.online </app/schema/${component}.js &>>${log_file}
+    status_check $?
+}
+nodejs(){
   print_head "Configure NodeJS Repo"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
   status_check $?
@@ -71,16 +85,6 @@ NODEJS(){
   systemctl start ${component} &>>${log_file}
   status_check $?
 
-  print_head "Copy MongoDB Repo File "
-  cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
-  status_check $?
-
-  print_head "Install Mongo Client"
-  yum install mongodb-org-shell -y &>>${log_file}
-  status_check $?
-
-  print_head "Load Schema"
-  mongo --host mongodb.devopsculture.online </app/schema/${component}.js &>>${log_file}
-  status_check $?
+  schema_setup
 
 }
